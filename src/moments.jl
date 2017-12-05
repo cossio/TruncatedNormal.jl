8ed390a27d009fad339d6312f5b9fbeb9725df83
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2017-12-05 16:00:04 +0000

    test var

--- src/moments.jl
+++ src/moments.jl
@@ -1,5 +1,48 @@
-tnmean(a, b) = √(2/π) * F1(a/√2, b/√2)
-tnmean(a, b, μ, σ) = μ + tnmean(a,b) * σ
+export tnmean, tnvar
 
-tnvar(a,b) = 1 + 2/√π * F2(a/√2, b/√2) - 2/π * F1(a/√2, b/√2)^2
-tnvar(a,b,μ,σ) = tnvar(a,b) * σ^2
+"""
+    tnmean(a,b)
+Mean of the truncated standard normal distribution
+
+    tnmean(a, b, μ, σ)
+Mean of the truncated normal distribution, where μ,σ
+are the mean and standard deviation of the untruncated
+distribution.
+"""
+function tnmean end
+
+function tnmean(a, b)
+    -Inf < a ≤ b < Inf || throw(DomainError())
+    √(2/π) * F1(a/√2, b/√2)
+end
+
+function tnmean(a, b, μ, σ)
+    -Inf < a ≤ b < Inf || throw(DomainError())
+    -Inf < μ < Inf && 0 < σ < Inf || throw(DomainError())
+    α = (a - μ)/σ; β = (b - μ)/σ
+    μ + tnmean(α, β) * σ
+end
+
+
+"""
+    tnvar(a,b)
+Variance of the truncated standard normal distribution
+
+    tnvar(a, b, μ, σ)
+Variance of the truncated normal distribution, where μ,σ
+are the mean and standard deviation of the untruncated
+distribution.
+"""
+function tnvar end
+
+function tnvar(a, b)
+    -Inf < a ≤ b < Inf || throw(DomainError())
+    1 + 2/√π * F2(a/√2, b/√2) - 2/π * F1(a/√2, b/√2)^2
+end
+
+function tnvar(a, b, μ, σ)
+    -Inf < a ≤ b < Inf || throw(DomainError())
+    -Inf < μ < Inf && 0 < σ < Inf || throw(DomainError())
+    α = (a - μ)/σ; β = (b - μ)/σ
+    tnvar(α, β) * σ^2
+end
