d41cdfa955dbc63b63e2b580bd9fb6cabc788038
Jorge Fernández de Cossío Díaz <j.cossio.diaz@gmail.com>
2018-06-06 15:10:31 +0200

    R, reorder function arguments to be consistent with other functions

--- src/bidimensional/R/bivariate-R.jl
+++ src/bidimensional/R/bivariate-R.jl
@@ -14,7 +14,7 @@ RealVector = AbstractVector{<:Real}
 Samples N points from a bivariate Gaussian X ~ N(0,Sig) 
 conditional on lb ≤ X ≤ ub. Accepts infinite lb, ub.
 """
-function sample_truncated_bivariate_gaussian(lb::RealVector, ub::RealVector, Sig::RealMatrix, N::Integer)
+function sample_truncated_bivariate_gaussian(Sig::RealMatrix, lb::RealVector, ub::RealVector, N::Integer)
     #=
     This makes a call to the Z. I. Botev's R package TruncatedNormal.
     =#
@@ -41,14 +41,14 @@ end
 Samples N points from a bivariate Gaussian X ~ N(μ,Sig) 
 conditional on lb ≤ X ≤ ub. Accepts infinite lb, ub.
 """
-function sample_truncated_bivariate_gaussian(lb::RealVector, ub::RealVector, μ::RealVector, Sig::RealMatrix, N::Integer)
+function sample_truncated_bivariate_gaussian(μ::RealVector, Sig::RealMatrix, lb::RealVector, ub::RealVector, N::Integer)
     @assert size(Sig) == (2,2) && length(μ) == length(lb) == length(ub) == 2
     @assert N > 0
     @assert all(lb .< ub)
     @assert all(-Inf .< μ .< Inf)
     @assert issymmetric(Sig)
 
-    X = sample_truncated_bivariate_gaussian(lb .- μ, ub .- μ, Sig, N)
+    X = sample_truncated_bivariate_gaussian(Sig, lb .- μ, ub .- μ, N)
     return X .+ μ
 end
 
@@ -57,8 +57,8 @@ end
 Compute the mean and covariance matrix of a Gaussian
 bivariate X ~ N(μ,Σ), conditional on lb < X < ub.
 """
-function truncated_bivariate_gaussian_moments(lb::RealVector, ub::RealVector, μ::RealVector, Σ::RealMatrix, N::Integer = 10000)
-    X = sample_truncated_bivariate_gaussian(lb, ub, μ, Σ, N)
+function truncated_bivariate_gaussian_moments(μ::RealVector, Σ::RealMatrix, lb::RealVector, ub::RealVector, N::Integer = 10000)
+    X = sample_truncated_bivariate_gaussian(μ, Σ, lb, ub, N)
     tμ = mean(X, 2)
     tΣ = [mean(X[i,:], X[j,:]) for i = 1:2, j = 1:2]
     return tμ, tΣ
