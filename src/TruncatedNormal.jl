fc904152f2da11a257e3ccdd3e49ef118b81d437
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2018-11-12 12:45:19 -0500

    forget bidimensional

--- src/TruncatedNormal.jl
+++ src/TruncatedNormal.jl
@@ -3,11 +3,11 @@ module TruncatedNormal
 include("F.jl")
 include("moments.jl")
 
-include("bidimensional/_bidimensional_common.jl")
-include("bidimensional/_ellipse_rect_minmax_2D.jl")
-include("bidimensional/_integrate_gauss_2D.jl")
-include("bidimensional/gauss_2D_moments.jl")
+#include("bidimensional/_bidimensional_common.jl")
+#include("bidimensional/_ellipse_rect_minmax_2D.jl")
+#include("bidimensional/_integrate_gauss_2D.jl")
+#include("bidimensional/gauss_2D_moments.jl")
 
-include("bidimensional/R/bivariate-R.jl")
+#include("bidimensional/R/bivariate-R.jl")
 
 end # module
