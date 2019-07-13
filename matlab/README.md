d5541b025a4622932ec1da2ed9a1cc1111c0e832
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2019-07-13 12:40:51 -0400

    remove bivariate

--- matlab/README.md
+++
@@ -1,4 +0,0 @@
-I obtained the Matlab packages from:
-
-https://uk.mathworks.com/matlabcentral/fileexchange/53792-truncated-multivariate-normal-generator
-https://uk.mathworks.com/matlabcentral/fileexchange/53180-truncated-normal-generator
--- src/bidimensional/_bidimensional_common.jl
+++
@@ -1,9 +0,0 @@
-function _ellipse_quadratic(ρ::Real, x::Tuple{Real,Real})::Float64
-    return x[1]^2 + x[2]^2 - 2ρ * x[1] * x[2]
-end
-
-function _log_normal_pdf(ρ::Real, x::Tuple{Real,Real})::Float64
-    @assert -1 < ρ < 1
-    #return -_ellipse_quadratic(ρ, x) / (2 * (1 - ρ^2)) - log(2π * sqrt(1 - ρ^2))
-    return -_ellipse_quadratic(ρ, x) / (2 * (1 - ρ) * (1 + ρ)) - log(2π) - 0.5log(1 - ρ) - 0.5log(1 + ρ)
-end
