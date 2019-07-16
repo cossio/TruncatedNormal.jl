d6a98756558edecbe6b578a6708f8e857ae1ded3
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2019-07-15 20:32:32 -0400

    some corrections (tests passing); removed Taylor expansion because it wasn't needed

--- src/F.jl
+++ src/F.jl
@@ -1,63 +1,74 @@
-using SpecialFunctions
+using Statistics, SpecialFunctions
 
 
 """
-_F1(x, y) =
-
-    (exp(-x^2) - exp(-y^2)) / (erf(y) - erf(x))
+    _F1(x, y) = (exp(-y^2) - exp(-x^2)) / (erf(y) - erf(x))
 
 without catastrophic cancellation. _F1(±∞,±∞) is defined
 by taking the limit of the second argument first.
 """
-function _F1(x::Real, y::Real; thresh=1e-7)  
-    if abs(x) > abs(y)
+function _F1(x::Real, y::Real)
+    if x > y && abs(x) > abs(y)
         return _F1(y, x)
-    elseif isinf(y)
-        return sign(y) / erfcx(sign(y) * x)
-    elseif abs(x - y) ≤ thresh
-        ϵ = y - x
-        return √π*x + (√π/2 + (-√π*x/6 + (-√π/12 + x*(√π/90 + (√π*x^2)/90)ϵ)ϵ)ϵ)ϵ
+    elseif x > y && abs(x) ≤ abs(y)
+        return -_F1(-x, -y)
+    elseif x ≤ y && abs(x) > abs(y)
+        return -_F1(-y, -x)
     end
     
-    Δ = exp(x^2 - y^2)
+    @assert x ≤ y && abs(x) ≤ abs(y)
+
+    if x == y
+        return -√π * x
+    elseif isinf(y)
+        return -sign(y) / erfcx(sign(y) * x)
+    end
+    
+    Δm1 = expm1((x - y) * (x + y))
+    Δ = one(Δm1) + Δm1
 
     if max(x, y) < 0
-        (1 - Δ) / (Δ * erfcx(-y) - erfcx(-x))
+        Δm1 / (Δ * erfcx(-y) - erfcx(-x))
     elseif min(x, y) > 0 || y == Inf
-        (1 - Δ) / (erfcx(x) - Δ * erfcx(y))
+        Δm1 / (erfcx(x) - Δ * erfcx(y))
     else
-        exp(-x^2) * (1 - Δ) / (erf(y) - erf(x))
+        Δm1 * exp(-x^2) / (erf(y) - erf(x))
     end
 end
 
-
 """
-_F2(x, y) =
-
-    (x * exp(-x^2) - y * exp(-y^2)) / (erf(y) - erf(x))
+    _F2(x, y) = (y * exp(-y^2) - x * exp(-x^2)) / (erf(y) - erf(x))
 
 without catastrophic cancellation. _F2(±∞,±∞) is defined
 by taking the limit in the second argument first.
 """
-function _F2(x::Real, y::Real; thresh=1e-7)::Float64
-    if abs(x) > abs(y)
+function _F2(x::Real, y::Real)::Float64
+    if x > y && abs(x) > abs(y)
         return _F2(y, x)
-    elseif x == Inf && y == -Inf || x == -Inf && y == Inf
-        return 0.
-    elseif isinf(y)
-        return sign(y) * x / erfcx(sign(y) * x)
-    elseif abs(x - y) ≤ thresh
-        ϵ = y - x
-        return √π*x^2 - √π/2 + (√π*x + (√π/3 - √π*x^2/3 + (((√π/30 + √π*x^2/45)x^2 - 4*√π/45)ϵ - √π*x/3)ϵ)ϵ)ϵ
+    elseif x > y && abs(x) ≤ abs(y)
+        return _F2(-x, -y)
+    elseif x ≤ y && abs(x) > abs(y)
+        return _F2(-y, -x)
     end
 
-    Δ = exp(x^2 - y^2)
-    
+    @assert x ≤ y && abs(x) ≤ abs(y)
+
+    if x == Inf && y == -Inf || x == -Inf && y == Inf
+        return zero(x + y)
+    elseif isinf(y)
+        return -sign(y) * x / erfcx(sign(y) * x)
+    elseif x == y
+        return √π * (1 - 2x^2) / 2
+    end
+
+    Δm1 = expm1(x^2 - y^2)
+    Δ = Δm1 + 1
+
     if max(x, y) < 0
-        (x - Δ * y) / (Δ * erfcx(-y) - erfcx(-x))
+        (Δ * y - x) / (Δ * erfcx(-y) - erfcx(-x))
     elseif min(x, y) > 0
-        (x - Δ * y) / (erfcx(x) - Δ * erfcx(y))
+        (Δ * y - x) / (erfcx(x) - Δ * erfcx(y))
     else
-        exp(-x^2) * (x - Δ * y) / (erf(y) - erf(x))
+        exp(-x^2) * (Δ * y - x) / (erf(y) - erf(x))
     end
 end
--- src/moments.jl
+++ src/moments.jl
@@ -7,7 +7,7 @@ Mean of the truncated standard normal distribution.
 """
 function tnmean(a, b)
     if a < b
-        √(2/π) * _F1(a / √2, b / √2)
+        -√(2/π) * _F1(a / √2, b / √2)
     elseif a == b
         a
     else
@@ -46,15 +46,7 @@ tnmom1 = tnmean
 
 Second moment of the truncated standard normal distribution.
 """
-function tnmom2(a, b)
-    if a < b
-        1 + (2/√π) * _F2(a / √2, b / √2)
-    elseif a == b
-        a^2
-    else
-        throw(ArgumentError("a must be < b; got a = $a, b = $b"))
-    end
-end
+tnmom2(a, b) = tnmom1(a,b)^2 + tnvar(a, b)
 
 """
     tnmom2(a, b, μ, σ)
@@ -62,25 +54,24 @@ end
 Second moment of the truncated normal distribution, where μ, σ
 are the mean and standard deviation of the untruncated distribution.
 """
-function tnmom2(a, b, μ, σ)
-    α = (a - μ) / σ
-    β = (b - μ) / σ
-    μ * (μ + 2σ * tnmom1(α, β)) + tnmom2(α, β) * σ^2
-end
+tnmom2(a, b, μ, σ) = tnmom1(a, b, μ, σ)^2 + tnvar(a, b, μ, σ)
 
 """
-    tnvar(a,b)
+    tnvar(a, b)
 
 Variance of the truncated standard normal distribution
 """
 function tnvar(a, b)
-    return 1 + 2/√π * _F2(a/√2, b/√2) - 2/π * _F1(a/√2, b/√2)^2
-    if a < b
-        1 + 2/√π * _F2(a/√2, b/√2) - 2/π * _F1(a/√2, b/√2)^2
-    elseif a == b
-        a
+    if a == b
+        return zero(a + b)
+    elseif a < b
+        if abs(a) < abs(b)
+            return tnvar(-b, -a)
+        else
+            return 1 - 2/√π * _F2(a/√2, b/√2) - tnmean(a, b)^2
+        end
     else
-        throw(ArgumentError("a must be ≤ b"))
+        throw(ArgumentError("a must be ≤ b; got a = $a, b = $b"))
     end
 end
 
@@ -91,6 +82,7 @@ Variance of the truncated normal distribution, where μ, σ are the mean and
 standard deviation of the untruncated distribution.
 """
 function tnvar(a, b, μ, σ)
-    α = (a - μ)/σ; β = (b - μ)/σ
+    α = (a - μ) / σ
+    β = (b - μ) / σ
     return tnvar(α, β) * σ^2
 end
